e58b4a040f843eb1cd64173631fc4a6b23619e74
Khaled Nowara <KN_Nowara@outlook.com>
2020-12-29 03:05:43 +0200

    Project with video and inital landing page

---
+++ Assets/SelectButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectButton : MonoBehaviour
+{
+    public void StartAr() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+    }
+   
+    
+}
